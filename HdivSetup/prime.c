f3536263292eeba500d176271ee00e40e5dc1706
Nathan Collier <nathaniel.collier@gmail.com>
2020-10-22 13:12:10 -0400

    implemented an evaluator of my prime basis

--- HdivSetup/prime.c
+++ HdivSetup/prime.c
@@ -1,42 +1,64 @@
 #include <petsc.h>
 
 /*
-  Routine is written so you can comment out a basis row to explore
-  effects of removing certain enrichments.
+  This basis was constructed by (1) starting with the hexahedral basis
+  found in (Wheeler,Xue,Yotov 2012), but omitting the addition of
+  curl(0,0,x*y^2) and curl(0,0,x^2*y*z) to the BDDF1 space, (2)
+  creating a constraint matrix to remove x*y from the top and bottom
+  faces of the prism and higher order terms from the diagonal face (3)
+  taking the SVD and using the eigenvectors to constrain the original
+  space.
 
-  Input:
-    x, point at which to evaluate the prime basis
-  Output:
-    n, number of basis functions
-    P, prime basis of size (basis,component) in row-major
- */
-void PrimeBasisWheelerXueYotov(PetscReal *x,PetscInt *n,PetscReal *P){
-  PetscInt i=0;
-  P[i] =    1; P[i+1] =    0; P[i+2] =    0; i+=3;
-  P[i] = x[0]; P[i+1] =    0; P[i+2] =    0; i+=3;
-  P[i] = x[1]; P[i+1] =    0; P[i+2] =    0; i+=3;
-  P[i] = x[2]; P[i+1] =    0; P[i+2] =    0; i+=3;
-  P[i] =    0; P[i+1] =    1; P[i+2] =    0; i+=3;
-  P[i] =    0; P[i+1] = x[0]; P[i+2] =    0; i+=3;
-  P[i] =    0; P[i+1] = x[1]; P[i+2] =    0; i+=3;
-  P[i] =    0; P[i+1] = x[2]; P[i+2] =    0; i+=3;
-  P[i] =    0; P[i+1] =    0; P[i+2] =    1; i+=3;
-  P[i] =    0; P[i+1] =    0; P[i+2] = x[0]; i+=3;
-  P[i] =    0; P[i+1] =    0; P[i+2] = x[1]; i+=3;
-  P[i] =    0; P[i+1] =    0; P[i+2] = x[2]; i+=3;
-  P[i] =         x[0]*x[2]; P[i+1] =        -x[1]*x[2]; P[i+2] =                 0; i+=3; /* curl(0       ,0       ,x*y*z   ) */
-  //P[i] =     2*x[0]*x[1]; P[i+1] =        -x[1]*x[1]; P[i+2] =                 0; i+=3; /* curl(0       ,0       ,x*y**2  ) */
-  P[i] =                 0; P[i+1] =      -2*x[0]*x[2]; P[i+2] =                 0; i+=3; /* curl(0       ,0       ,x**2*z  ) */
-  //P[i] =  x[0]*x[0]*x[2]; P[i+1] = -2*x[0]*x[1]*x[2]; P[i+2] =                 0; i+=3; /* curl(0       ,0       ,x**2*y*z) */
-  P[i] =                 0; P[i+1] =         x[0]*x[1]; P[i+2] =        -x[0]*x[2]; i+=3; /* curl(x*y*z   ,0       ,0       ) */
-  P[i] =                 0; P[i+1] =       2*x[1]*x[2]; P[i+2] =        -x[2]*x[2]; i+=3; /* curl(y*z**2  ,0       ,0       ) */
-  P[i] =                 0; P[i+1] =                 0; P[i+2] =      -2*x[0]*x[1]; i+=3; /* curl(x*y**2  ,0       ,0       ) */
-  P[i] =                 0; P[i+1] =    x[0]*x[1]*x[1]; P[i+2] = -2*x[0]*x[1]*x[2]; i+=3; /* curl(x*y**2*z,0       ,0       ) */
-  P[i] =        -x[0]*x[1]; P[i+1] =                 0; P[i+2] =         x[1]*x[2]; i+=3; /* curl(0       ,x*y*z   ,0       ) */
-  P[i] =        -x[0]*x[0]; P[i+1] =                 0; P[i+2] =       2*x[0]*x[2]; i+=3; /* curl(0       ,x**2*z  ,0       ) */
-  P[i] =      -2*x[1]*x[2]; P[i+1] =                 0; P[i+2] =                 0; i+=3; /* curl(0       ,y*z**2  ,0       ) */
-  P[i] = -2*x[0]*x[1]*x[2]; P[i+1] =                 0; P[i+2] =    x[1]*x[2]*x[2]; i+=3; /* curl(0       ,x*y*z**2,0       ) */
-  (*n) = i/3;
+  p in [0, npoints), i in [0, pdim), c in [0, Nc)
+
+  B[p][i][c] = B[p][i_scalar][c][c]
+
+*/
+static PetscErrorCode PetscSpaceEvaluate_HdivPrism(PetscSpace sp, PetscInt npoints, const PetscReal points[], PetscReal B[], PetscReal D[], PetscReal H[])
+{
+  PetscInt p, dim = sp->Nv;
+  PetscReal x, y, z;
+  PetscFunctionBegin;
+  if (D || H) {
+    SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_SUP,"Derivatives and Hessians not supported for Hdiv spaces on prisms");
+  }
+  if (dim != 3) {
+    SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_SUP,"Prisms only defined for spatial dimension = 3");
+  }
+  if (sp->Nc != 3) {
+    SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_SUP,"Hdiv prism space only defined for number of components = 3");
+  }  
+  for (p = 0; p < npoints; p++) {
+    x = points[p*dim];
+    y = points[p*dim+1];
+    z = points[p*dim+2];
+    B[54*p+ 1] =  1;
+    B[54*p+ 4] =  x;
+    B[54*p+ 7] =  y;
+    B[54*p+10] =  z;
+    B[54*p+14] =  1;
+    B[54*p+20] =  y;
+    B[54*p+23] =  z;
+    B[54*p+24] =  x*z;
+    B[54*p+25] = -y*z;
+    B[54*p+28] = -2*x*z;
+    B[54*p+30] =  0.333333333333333*x*x - 0.333333333333333*x*y + 0.577350269189626*y;
+    B[54*p+31] =  0.666666666666667*x*y;
+    B[54*p+32] = -1.33333333333333*x*z + 0.333333333333333*y*z;
+    B[54*p+34] =  2*y*z;
+    B[54*p+35] = -z*z;
+    B[54*p+36] = -0.707106781186547*x + 0.707106781186547;
+    B[54*p+39] =  0.707106781186547*x + 0.707106781186547;
+    B[54*p+42] = -0.333333333333333*x*x - 0.666666666666667*x*y - 0.577350269189626*y;
+    B[54*p+43] =  0.333333333333333*x*y;
+    B[54*p+44] =  0.333333333333333*x*z + 0.666666666666667*y*z;
+    B[54*p+45] = -0.666666666666667*x*x - 0.333333333333333*x*y + 0.577350269189626*y;
+    B[54*p+46] = -0.333333333333333*x*y;
+    B[54*p+47] =  1.666666666666667*x*z + 0.333333333333333*y*z;
+    B[54*p+48] = -2*y*z;
+    B[54*p+51] =  z; 
+  }
+  PetscFunctionReturn(0);
 }
 
 int main(int argc, char **argv)
@@ -44,11 +66,6 @@ int main(int argc, char **argv)
   PetscErrorCode ierr;
   ierr = PetscInitialize(&argc,&argv,NULL,NULL);CHKERRQ(ierr);
 
-  PetscReal x[3],P[72];
-  PetscInt i,n;
-  x[0] = 0.25; x[1] = -0.12; x[2] = 0.9;
-  PrimeBasisWheelerXueYotov(x,&n,P);
-  for(i=0;i<3*n;i++) printf("P[%2d] = %+e\n",i,P[i]);
   
   ierr = PetscFinalize();CHKERRQ(ierr);
   return ierr;
