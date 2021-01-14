b9163cc8cf27e9d96d2685d903bcf6427757676e
Rezgar Shakeri <Rezgar.Shakeri@colorado.edu>
2021-01-13 21:31:56 -0700

    test of Piola, Normal, Nodal Basis and Divergence of Nodal basis passed

--- ACspace/test_FE_subroutines2.py
+++ ACspace/test_FE_subroutines2.py
@@ -7,14 +7,14 @@ class TestPiolaTransformation(unittest.TestCase):
 
     def test_Piola1(self):
         """ Note 
-        2---3
+        3---2
         |   |
         0---1
         """
         coord_E = [[0.,0.],
                    [1.,0.],
-                   [0.,1.],
-                   [1.,1.]]
+                   [1.,1.],
+                   [0.,1.]]
         Xhat = [-1,-1]
         X, DF_E, J_E = FE.PiolaTransform(coord_E, Xhat)
 
@@ -27,14 +27,14 @@ class TestPiolaTransformation(unittest.TestCase):
 
     def test_Piola2(self):
         """ Note 
-        2---3
+        3---2
         |   |
         0---1
         """
         coord_E = [[0.,0.],
                    [1.,0.],
-                   [0.,1.],
-                   [1.,1.]]
+                   [1.,1.],
+                   [0.,1.]]
         Xhat = [0., 0.]
         X, DF_E, J_E = FE.PiolaTransform(coord_E, Xhat)
 
@@ -52,8 +52,8 @@ class TestNormal(unittest.TestCase):
         # test on E = [0,0.5]^2
         coord_E = [[0.0,0.0],
                    [0.5,0.0],
-                   [0.0,0.5],
-                   [0.5,0.5]]
+                   [0.5,0.5],
+                   [0.0,0.5]]
         # check the left edge and (x,y) mapped from (xhat,yhat)
         nl = [-1.,0.]
         n, X = FE.GetNormal(coord_E,[-1. ,0.])
@@ -85,8 +85,8 @@ class TestNormal2(unittest.TestCase):
     def test_Normal2(self):
         coord_E = [[0.,0.],
                    [1.,0.],
-                   [0.25,0.5],
-                   [0.75,0.75]]
+                   [0.75,0.75],
+                   [0.25,0.5]]
         # check the left edge normal and middle point (x,y) mapped from (xhat,yhat)
         nl = np.array([-2/math.sqrt(5), 1/math.sqrt(5)])
         # enter the middle point (-1,,0) on the left edge of Ehat
@@ -131,68 +131,72 @@ class TestNodalBasisUniform(unittest.TestCase):
         """
         coord_E = [[0.,0.],
                    [1.,0.],
-                   [0.,0.25],
-                   [1.,0.25]]
+                   [1.,0.25],
+                   [0.,0.25]]
         nl, X = FE.GetNormal(coord_E, [-1., 0.])
         nr, X = FE.GetNormal(coord_E, [1., 0.])
         nb, X = FE.GetNormal(coord_E, [0., -1.])
         nt, X = FE.GetNormal(coord_E, [0., 1.])
         # check node 1,
         Nhat = FE.GetACNodalBasis(coord_E,[-1,-1])
-        # check v11.nl=1 and v12.nb=1
-        self.assertAlmostEqual(np.dot(Nhat[:,0],nl),1.0,None,None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,1],nb),1.0,None,None,1e-10)
-        # check other nodes v21.nr = 0,...
+        # Note: Nhat = [v11,v12,v21,v22,v31,v32,v41,v42]
+        # check v11.nb=1 and v42.nl=1
+        self.assertAlmostEqual(np.dot(Nhat[:,0],nb),1.0,None,None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,7],nl),1.0,None,None,1e-10)
+        # check other nodes
+        self.assertAlmostEqual(np.dot(Nhat[:,1],nb),0.0, None, None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,2],nr),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,3],nb),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,4],nl),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,3],nr),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,4],nt),0.0, None, None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,5],nt),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,6],nr),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,7],nt),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,6],nl),0.0, None, None,1e-10)
 
         # check node 2,
         Nhat = FE.GetACNodalBasis(coord_E,[1,-1])
-        # check v21.nr=1 and v22.nb=1 
+        # Note: Nhat = [v11,v12,v21,v22,v31,v32,v41,v42]
+        # check v12.nb=1 and v21.nr=1 
+        self.assertAlmostEqual(np.dot(Nhat[:,1],nb),1.0,None,None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,2],nr),1.0,None,None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,3],nb),1.0,None,None,1e-10)
-        # check other nodes v11.nl = 0
-        self.assertAlmostEqual(np.dot(Nhat[:,0],nl),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,1],nb),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,4],nl),0.0, None, None,1e-10)
+        # check other nodes
+        self.assertAlmostEqual(np.dot(Nhat[:,0],nb),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,3],nr),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,4],nt),0.0, None, None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,5],nt),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,6],nr),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,7],nt),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,6],nl),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,7],nl),0.0, None, None,1e-10)
 
         # check node 3,
-        Nhat = FE.GetACNodalBasis(coord_E,[-1,1])
-        # check v31.nl=1 and v32.nt=1 
-        self.assertAlmostEqual(np.dot(Nhat[:,4],nl),1.0,None,None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,5],nt),1.0,None,None,1e-10)
-        # check other nodes v41.nr = 0
-        self.assertAlmostEqual(np.dot(Nhat[:,0],nl),0.0, None, None,1e-10)
+        Nhat = FE.GetACNodalBasis(coord_E,[1,1])
+        # Note: Nhat = [v11,v12,v21,v22,v31,v32,v41,v42]
+        # check v22.nr=1 and v31.nt=1 
+        self.assertAlmostEqual(np.dot(Nhat[:,3],nr),1.0,None,None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,4],nt),1.0,None,None,1e-10)
+        # check other nodes 
+        self.assertAlmostEqual(np.dot(Nhat[:,0],nb),0.0, None, None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,1],nb),0.0, None, None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,2],nr),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,3],nb),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,6],nr),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,7],nt),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,5],nt),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,6],nl),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,7],nl),0.0, None, None,1e-10)
 
         # check node 4,
-        Nhat = FE.GetACNodalBasis(coord_E,[1,1])
-        # check v41.nr=1 and v42.nt=1 
-        self.assertAlmostEqual(np.dot(Nhat[:,6],nr),1.0,None,None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,7],nt),1.0,None,None,1e-10)
-        # check other nodes v31.nl = 0
-        self.assertAlmostEqual(np.dot(Nhat[:,0],nl),0.0, None, None,1e-10)
+        Nhat = FE.GetACNodalBasis(coord_E,[-1,1])
+        # Note: Nhat = [v11,v12,v21,v22,v31,v32,v41,v42]
+        # check v32.nt=1 and v41.nl=1 
+        self.assertAlmostEqual(np.dot(Nhat[:,5],nt),1.0,None,None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,6],nl),1.0,None,None,1e-10)
+        # check other nodes
+        self.assertAlmostEqual(np.dot(Nhat[:,0],nb),0.0, None, None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,1],nb),0.0, None, None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,2],nr),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,3],nb),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,4],nl),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,5],nt),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,3],nr),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,4],nt),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,7],nl),0.0, None, None,1e-10)
 
 
 class TestNodalBasisNonUniform(unittest.TestCase):
 
-    #check Nhat the Nodal basis on non-uniform mesh
+    #check Nhat, the Nodal basis on uniform mesh
     def test_GetNodalBasis2(self):
         """ element E is taken from fig 3.5 of Zhen Tao PhD thesis
                  4
@@ -204,63 +208,68 @@ class TestNodalBasisNonUniform(unittest.TestCase):
         """
         coord_E = [[0.,0.],
                    [1.,0.],
-                   [0.25,0.5],
-                   [0.75,0.75]]
+                   [0.75,0.75],
+                   [0.25,0.5]]
         nl, X = FE.GetNormal(coord_E, [-1., 0.])
         nr, X = FE.GetNormal(coord_E, [1., 0.])
         nb, X = FE.GetNormal(coord_E, [0., -1.])
         nt, X = FE.GetNormal(coord_E, [0., 1.])
         # check node 1,
         Nhat = FE.GetACNodalBasis(coord_E,[-1,-1])
-        # check v11.nl=1 and v12.nb=1
-        self.assertAlmostEqual(np.dot(Nhat[:,0],nl),1.0,None,None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,1],nb),1.0,None,None,1e-10)
-        # check other nodes v21.nr = 0,...
+        # Note: Nhat = [v11,v12,v21,v22,v31,v32,v41,v42]
+        # check v11.nb=1 and v42.nl=1
+        self.assertAlmostEqual(np.dot(Nhat[:,0],nb),1.0,None,None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,7],nl),1.0,None,None,1e-10)
+        # check other nodes
+        self.assertAlmostEqual(np.dot(Nhat[:,1],nb),0.0, None, None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,2],nr),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,3],nb),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,4],nl),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,3],nr),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,4],nt),0.0, None, None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,5],nt),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,6],nr),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,7],nt),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,6],nl),0.0, None, None,1e-10)
 
         # check node 2,
         Nhat = FE.GetACNodalBasis(coord_E,[1,-1])
-        # check v21.nr=1 and v22.nb=1 
+        # Note: Nhat = [v11,v12,v21,v22,v31,v32,v41,v42]
+        # check v12.nb=1 and v21.nr=1 
+        self.assertAlmostEqual(np.dot(Nhat[:,1],nb),1.0,None,None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,2],nr),1.0,None,None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,3],nb),1.0,None,None,1e-10)
-        # check other nodes v11.nl = 0
-        self.assertAlmostEqual(np.dot(Nhat[:,0],nl),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,1],nb),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,4],nl),0.0, None, None,1e-10)
+        # check other nodes
+        self.assertAlmostEqual(np.dot(Nhat[:,0],nb),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,3],nr),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,4],nt),0.0, None, None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,5],nt),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,6],nr),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,7],nt),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,6],nl),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,7],nl),0.0, None, None,1e-10)
 
         # check node 3,
-        Nhat = FE.GetACNodalBasis(coord_E,[-1,1])
-        # check v31.nl=1 and v32.nt=1 
-        self.assertAlmostEqual(np.dot(Nhat[:,4],nl),1.0,None,None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,5],nt),1.0,None,None,1e-10)
-        # check other nodes v41.nr = 0
-        self.assertAlmostEqual(np.dot(Nhat[:,0],nl),0.0, None, None,1e-10)
+        Nhat = FE.GetACNodalBasis(coord_E,[1,1])
+        # Note: Nhat = [v11,v12,v21,v22,v31,v32,v41,v42]
+        # check v22.nr=1 and v31.nt=1 
+        self.assertAlmostEqual(np.dot(Nhat[:,3],nr),1.0,None,None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,4],nt),1.0,None,None,1e-10)
+        # check other nodes 
+        self.assertAlmostEqual(np.dot(Nhat[:,0],nb),0.0, None, None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,1],nb),0.0, None, None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,2],nr),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,3],nb),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,6],nr),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,7],nt),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,5],nt),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,6],nl),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,7],nl),0.0, None, None,1e-10)
 
         # check node 4,
-        Nhat = FE.GetACNodalBasis(coord_E,[1,1])
-        # check v41.nr=1 and v42.nt=1 
-        self.assertAlmostEqual(np.dot(Nhat[:,6],nr),1.0,None,None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,7],nt),1.0,None,None,1e-10)
-        # check other nodes v31.nl = 0
-        self.assertAlmostEqual(np.dot(Nhat[:,0],nl),0.0, None, None,1e-10)
+        Nhat = FE.GetACNodalBasis(coord_E,[-1,1])
+        # Note: Nhat = [v11,v12,v21,v22,v31,v32,v41,v42]
+        # check v32.nt=1 and v41.nl=1 
+        self.assertAlmostEqual(np.dot(Nhat[:,5],nt),1.0,None,None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,6],nl),1.0,None,None,1e-10)
+        # check other nodes
+        self.assertAlmostEqual(np.dot(Nhat[:,0],nb),0.0, None, None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,1],nb),0.0, None, None,1e-10)
         self.assertAlmostEqual(np.dot(Nhat[:,2],nr),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,3],nb),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,4],nl),0.0, None, None,1e-10)
-        self.assertAlmostEqual(np.dot(Nhat[:,5],nt),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,3],nr),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,4],nt),0.0, None, None,1e-10)
+        self.assertAlmostEqual(np.dot(Nhat[:,7],nl),0.0, None, None,1e-10)
+
 
 class TestDivNodalBasis(unittest.TestCase):
 
@@ -274,38 +283,40 @@ class TestDivNodalBasis(unittest.TestCase):
          /           \
         1-------------2
         """
-        coord_E = [[0.,0.],
-                   [1.,0.],
-                   [0.25,0.5],
-                   [0.75,0.75]]
+        coord_E = np.array([[0.,0.],
+                            [1.,0.],
+                            [0.75,0.75],
+                            [0.25,0.5]])
         nl, X = FE.GetNormal(coord_E, [-1., 0.])
         nr, X = FE.GetNormal(coord_E, [1., 0.])
         nb, X = FE.GetNormal(coord_E, [0., -1.])
         nt, X = FE.GetNormal(coord_E, [0., 1.])
         Dhat = FE.GetDivACNodalBasis(coord_E)
 
-        normals = np.block([[nl],[nb],[nr],[nb],[nl],[nt],[nr],[nt]])
+        normals = np.block([[nb],[nb],[nr],[nr],[nt],[nt],[nl],[nl]])
+        nodes = np.block([coord_E[0,:],coord_E[1,:],coord_E[1,:],coord_E[2,:],
+                          coord_E[2,:],coord_E[3,:],coord_E[3,:],coord_E[0,:]])
+        print(nodes)
         # test with u = [x-y,x+y] ==>div(u) = 2
         u = np.zeros((8,1))
-        for i in range(4):
-            x = coord_E[i][0]
-            y = coord_E[i][1]
-            u[2*i][0] = np.dot([x-y,x+y],normals[2*i,:])
-            u[2*i+1][0] = np.dot([x-y,x+y],normals[2*i+1,:])
-
+        for i in range(8):
+            x = nodes[2*i]
+            y = nodes[2*i+1]
+            u[i][0] = np.dot([x-y,x+y],normals[i,:])
+        print(Dhat)
+        print(u)
         const = Dhat @ u
-        self.assertEqual(const, 2.)
+        self.assertAlmostEqual(const[0][0],2.0, None, None,1e-10)
 
         # test with u = [-y,x] ==>div(u) = 0
         u = np.zeros((8,1))
-        for i in range(4):
-            x = coord_E[i][0]
-            y = coord_E[i][1]
-            u[2*i][0] = np.dot([-y,x],normals[2*i,:])
-            u[2*i+1][0] = np.dot([-y,x],normals[2*i+1,:])
+        for i in range(8):
+            x = nodes[2*i]
+            y = nodes[2*i+1]
+            u[i][0] = np.dot([-y,x],normals[i,:])
 
         const = Dhat @ u
-        self.assertEqual(const, 0.)
+        self.assertAlmostEqual(const[0][0],0.0, None, None,1e-10)
 
 
 def main():
