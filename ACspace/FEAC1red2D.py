ee910999b3c7f563a92222056d265676b42a3ea1
Rezgar Shakeri <Rezgar.Shakeri@colorado.edu>
2021-01-26 09:22:10 -0700

    added trig and quartic MMS

--- ACspace/FEAC1red2D.py
+++ ACspace/FEAC1red2D.py
@@ -1,17 +1,19 @@
 """ how to run
-python3 FEAC1red2D.py --nelx 3 --nely 3 --Q 3 --quadmethod 'LGL'
+python3 FEAC1red2D.py --nelx 4 --nely 4 --Q 2 --quadmethod 'GAUSS' --mesh 'uniform'
 if you run with 
 python3 FEAC1red2D.py
 uses the default values as
-nelx=2
-nely=2
+nelx=4
+nely=4
 Q=2
 quadmethod='GAUSS'
+mesh='uniform
 """
 import FEACSubroutines as FE
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.tri as tri
 
 def main():
     parser = argparse.ArgumentParser(description='Input for FE_subroutines')
@@ -19,13 +21,13 @@ def main():
     parser.add_argument('--nelx',
                         dest='nelx',
                         type=int,
-                        default=2,
+                        default=4,
                         help='Number of element in x direction')
 
     parser.add_argument('--nely',
                         dest='nely',
                         type=int,
-                        default=2,
+                        default=4,
                         help='Number of element in y direction')
 
     parser.add_argument('--Q',
@@ -46,6 +48,12 @@ def main():
                         default='uniform',
                         help='mesh distribution which can be:uniform, nonuniform, stretched, random')
 
+    parser.add_argument('--MMS',
+                        dest='MMS',
+                        type=str,
+                        default='quartic',
+                        help='MMS solution which can be:trig, quartic')
+
     args = parser.parse_args()
 
     nelx = args.nelx
@@ -53,16 +61,25 @@ def main():
     Q = args.Q
     quadmethod = args.quadmethod
     mesh = args.mesh
+    MMS = args.MMS
 
-    F, K = FE.Assembly(mesh, nelx, nely, Q, quadmethod)
+    F, K = FE.Assembly(MMS, mesh, nelx, nely, Q, quadmethod)
 
     dp, du = FE.GetFESol(K,F,nelx,nely)
-    p, u = FE.GetExactSol(mesh, nelx, nely)
+    p, u = FE.GetExactSol(MMS, mesh, nelx, nely)
+    error_u = np.absolute(du-u)
+    error_p = np.absolute(dp-p)
+    normerror_u = np.linalg.norm(du-u)
+    normerror_p = np.linalg.norm(dp-p)
+    print("norm of error of velocity is: ",normerror_u)
+    print("norm of error of pressure is: ",normerror_p)
+    #for i in range(len(u)):
+    #    print(i+1,'---',u[i,0],'====',du[i,0])
 
-    error_u = np.linalg.norm(du-u)
-    error_p = np.linalg.norm(dp-p)
-    print("norm of error of velocity is: ",error_u)
-    print("norm of error of pressure is: ",error_p)
+    FE.PltSolution(mesh, nelx, nely, u, p)
+    FE.PltSolution(mesh, nelx, nely, du, dp)
+    FE.PltSolution(mesh, nelx, nely, error_u, error_p)
+
     return
 
 if __name__ == '__main__':
