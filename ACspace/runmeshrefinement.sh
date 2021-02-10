05a24389e1fdd800ef5036db6155e136564cd493
Rezgar Shakeri <Rezgar.Shakeri@colorado.edu>
2021-02-10 09:01:13 -0700

    some clean up

--- ACspace/runmeshrefinement.sh
+++ ACspace/runmeshrefinement.sh
@@ -3,14 +3,14 @@ declare -A run_flags
     run_flags[nelx]=4
     run_flags[nely]=4
     run_flags[MMS]=quartic
-    run_flags[mesh]=uniform
+    run_flags[mesh]=random
 
 declare -A test_flags
     test_flags[res_start]=4
     test_flags[res_stride]=4
     test_flags[res_end]=16
 
-echo ",mesh_res,u_error,p_error" > conv_test_result.csv
+echo ",mesh_res,u_error,p_error" > ConvResult.csv
 i=0
 
 for ((res=${test_flags[res_start]}; res<=${test_flags[res_end]}; res+=${test_flags[res_stride]})); do
@@ -22,6 +22,6 @@ for ((res=${test_flags[res_start]}; res<=${test_flags[res_end]}; res+=${test_fla
             args="$args --$arg ${run_flags[$arg]}"
         fi
     done
-    python3 FEAC1red2D.py $args | grep "Velocity and Pressure Absolute Error:" | awk -v i="$i" -v res="$res" '{ print i","res","$6","$7}' >> conv_test_result.csv
+    python3 FEAC1red2D.py $args | grep "Velocity and Pressure Absolute Error:" | awk -v i="$i" -v res="$res" '{ print i","res","$6","$7}' >> ConvResult.csv
     i=$((i+1))
 done
