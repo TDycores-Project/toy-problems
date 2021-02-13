a06a46cd0a7fff6a121227918297851442500778
Rezgar Shakeri <Rezgar.Shakeri@colorado.edu>
2021-02-13 08:45:51 -0700

    to run inf-sup test

--- ACspace/runinfsup.sh
+++ ACspace/runinfsup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 declare -A run_flags
     run_flags[problem]=infsup
-    run_flags[nelx]=2
-    run_flags[nely]=2
-    run_flags[mesh]=uniform
+    run_flags[nelx]=4
+    run_flags[nely]=4
+    run_flags[mesh]=random
 
 declare -A test_flags
-    test_flags[res_start]=2
-    test_flags[res_stride]=2
-    test_flags[res_end]=10
+    test_flags[res_start]=4
+    test_flags[res_stride]=4
+    test_flags[res_end]=16
 
 fileName=InfSup.csv
-echo ",mesh_res,infsup" > $fileName
+echo ",mesh_res,coercivity,infsup" > $fileName
 i=0
 
 for ((res=${test_flags[res_start]}; res<=${test_flags[res_end]}; res+=${test_flags[res_stride]})); do
@@ -23,6 +23,6 @@ for ((res=${test_flags[res_start]}; res<=${test_flags[res_end]}; res+=${test_fla
             args="$args --$arg ${run_flags[$arg]}"
         fi
     done
-    python3 FEAC1red2D.py $args | grep "infsup constant:" | awk -v i="$i" -v res="$res" '{ print i","res","$3}' >> $fileName
+    python3 FEAC1red2D.py $args | grep "coercivity and infsup constants:" | awk -v i="$i" -v res="$res" '{ print i","res","$5","$6}' >> $fileName
     i=$((i+1))
 done
