07b15a35ac7947333cc6a5e23e48a5dde64a5a3b
rezgarshakeri <rezgar.shakeri@colorado.edu>
2021-07-19 05:06:17 -0600

    ACspace in PETSc: added GetJacobian function

--- ACspace/ACquad.c
+++ ACspace/ACquad.c
@@ -14,10 +14,12 @@ static char help[] =
 
 PetscErrorCode BilinearMap(PetscInt Q, PetscReal Coord_E[4][2], PetscReal xhat[], PetscReal yhat[], PetscReal *x, PetscReal *y);
 PetscErrorCode GetQuadrature2D(PetscInt Q1d, PetscReal *qx, PetscReal *qy, PetscReal *w);
+PetscErrorCode GetJacobian(PetscInt Q1d, PetscReal Coord_E[4][2], PetscReal xhat[], PetscReal yhat[],
+                           PetscReal *J11, PetscReal *J12, PetscReal *J21, PetscReal *J22, PetscReal *detJ);
 int main(int argc,char **argv) {
     PetscErrorCode ierr;
     PetscInt Q1d = 3;
-    PetscReal *qx, *qy, *w, *x, *y;
+    PetscReal *qx, *qy, *w, *x, *y, *J11, *J12, *J21, *J22, *detJ;
     PetscInt  i;
 
     ierr = PetscInitialize(&argc,&argv,NULL,help); if (ierr) return ierr;
@@ -26,17 +28,23 @@ int main(int argc,char **argv) {
     PetscMalloc1(Q1d*Q1d,&w);
     PetscMalloc1(Q1d*Q1d,&x);
     PetscMalloc1(Q1d*Q1d,&y);
+    PetscMalloc1(Q1d*Q1d,&J11);
+    PetscMalloc1(Q1d*Q1d,&J12);
+    PetscMalloc1(Q1d*Q1d,&J21);
+    PetscMalloc1(Q1d*Q1d,&J22);
+    PetscMalloc1(Q1d*Q1d,&detJ);
     ierr = GetQuadrature2D(Q1d, qx, qy, w); CHKERRQ(ierr);
     PetscReal Coord_E[4][2] = {{0., 0.},
                                {1., 0.},
                                {0., 1.},
                                {1., 1.}};
     ierr = BilinearMap(Q1d, Coord_E, qx, qy, x, y); CHKERRQ(ierr);
+    ierr = GetJacobian(Q1d, Coord_E, qx, qy, J11, J12, J21, J22, detJ);
     for (i = 0; i < Q1d*Q1d; i++){
     ierr = PetscPrintf(PETSC_COMM_WORLD,
         "x=%.5f ...\n"
         "y = %.5f\n",
-        x[i], y[i]); CHKERRQ(ierr);
+        detJ[i], J21[i]); CHKERRQ(ierr);
     }
     
     PetscFree(qx);
@@ -44,6 +52,11 @@ int main(int argc,char **argv) {
     PetscFree(w);
     PetscFree(x);
     PetscFree(y);
+    PetscFree(J11);
+    PetscFree(J12);
+    PetscFree(J21);
+    PetscFree(J22);
+    PetscFree(detJ);
     return PetscFinalize();
 }
 
@@ -70,6 +83,37 @@ PetscErrorCode BilinearMap(PetscInt Q1d, PetscReal Coord_E[4][2], PetscReal xhat
     return 0;
 }
 
+PetscErrorCode GetJacobian(PetscInt Q1d, PetscReal Coord_E[4][2], PetscReal xhat[], PetscReal yhat[],
+                           PetscReal *J11, PetscReal *J12, PetscReal *J21, PetscReal *J22, PetscReal *detJ) {
+    
+    PetscInt   i, j;
+
+    for (j = 0; j<Q1d*Q1d; j++) {
+        PetscReal dxdxhat = 0.0, dydxhat = 0.0, dxdyhat = 0.0, dydyhat = 0.0;
+        PetscReal GN_xhat[4], GN_yhat[4];
+        GN_xhat[0] = -0.25 * (1 - yhat[j]);
+        GN_xhat[1] =  0.25 * (1 - yhat[j]);
+        GN_xhat[2] = -0.25 * (1 + yhat[j]);
+        GN_xhat[3] =  0.25 * (1 + yhat[j]);
+
+        GN_yhat[0] = -0.25 * (1 - xhat[j]);
+        GN_yhat[1] = -0.25 * (1 + xhat[j]);
+        GN_yhat[2] =  0.25 * (1 - xhat[j]);
+        GN_yhat[3] =  0.25 * (1 + xhat[j]);
+
+            for (i = 0; i<4; i++) {
+                dxdxhat += GN_xhat[i] * Coord_E[i][0];  dydxhat += GN_xhat[i] * Coord_E[i][1];
+                dxdyhat += GN_yhat[i] * Coord_E[i][0];  dydyhat += GN_yhat[i] * Coord_E[i][1];
+            }
+    
+        detJ[j] = dxdxhat*dydyhat - dydxhat*dxdyhat;
+        J11[j] = dxdxhat;
+        J12[j] = dxdyhat;
+        J21[j] = dydxhat;
+        J22[j] = dydyhat;
+    }
+    return 0;
+}
 
 PetscErrorCode GetQuadrature2D(PetscInt Q1d, PetscReal *qx, PetscReal *qy, PetscReal *w) {
     
